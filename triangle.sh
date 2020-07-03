aff6447a214c96973788c1a5fbc0dfd6b5e39ea8
Sinthen <31857924+Sinthen@users.noreply.github.com>
2020-07-04 00:36:45 +0700

    Update triangle.sh

--- triangle.sh
+++ triangle.sh
@@ -22,8 +22,8 @@ ac_gt_b=$(echo "$a + $c > $b" | bc)
 ab_gt_c=$(echo "$a + $b > $c" | bc)
 
 if [ $bc_gt_a -eq 0 -o $ac_gt_b -eq 0 -o $ab_gt_c -eq 0 ]; then
-
 		echo "Не треугольник" >&2;
+		echo "Не треугольник";
 		exit 2
 fi
 
